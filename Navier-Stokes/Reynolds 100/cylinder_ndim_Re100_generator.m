087b98838484e260e1050857de344e797d1e5fe6
Morbidelli <59146785+Morbidelli@users.noreply.github.com>
2022-03-24 19:04:17 -0300

    Add files via upload

---
+++ Navier-Stokes/Reynolds 100/cylinder_ndim_Re100_generator.m
@@ -0,0 +1,26 @@
+%Código para criar o arquivo final referente à esteira de vórtices que
+%entra na PINN a partir dos arquivos ut.xlsx; vt.xlsx; p_star.xlsx; 
+%X_star.dat; t.dat.
+%========================================================
+
+%Importação das planilhas u e v como matrizes
+ut = readmatrix('ut.xlsx'); 
+vt = readmatrix('vt.xlsx'); 
+
+%transf. as matrizes 2D em matrizes 3D com 5000 linhas, 1 col e 200 de profundidade
+a = reshape (ut, [5000 1 200]); 
+b = reshape (vt, [5000 1 200]);
+
+%Concatenação das duas matrizes coluna gerando uma matriz 5000x2x200
+U_star = cat(2,a,b); 
+
+%Remoção das variávies intermediárias
+clear a b ut vt 
+
+%Importação dos valores de x, y, p, t 
+p_star = readmatrix('p_star.xlsx');
+load X_star.dat 
+load t.dat
+
+%Geração do arquivo final que deve ser inserido na PINN
+save('cylinder_ndim_Re100', 'U_star', 'X_star', 'p_star', 't'); 
